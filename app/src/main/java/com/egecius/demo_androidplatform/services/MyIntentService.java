0f63ebc48f91631cd2dc97a763f1b3017536a24f
Egidijus Gecius <e.gecius@gmail.com>
2017-11-08 12:36:38 +0000

    starts DetailActivity from IntentService

--- app/src/main/java/com/egecius/demo_androidplatform/services/MyIntentService.java
+++ app/src/main/java/com/egecius/demo_androidplatform/services/MyIntentService.java
@@ -1,27 +1,32 @@
 package com.egecius.demo_androidplatform.services;
 
+import static android.content.Intent.FLAG_ACTIVITY_NEW_TASK;
+
 import android.app.IntentService;
 import android.content.Intent;
 import android.support.annotation.Nullable;
 import android.util.Log;
-import android.widget.Toast;
+
+import com.egecius.demo_androidplatform.DetailActivity;
 
 public class MyIntentService extends IntentService {
 
     public static final String TAG = MyIntentService.class.getSimpleName();
 
     /**
-     * Creates an IntentService.  Invoked by your subclass's constructor.
-     * @param name Used to name the worker thread, important only for debugging.
+     * Creates an IntentService.  Invoked by your subclass's constructor - crashes if not provided
      */
     @SuppressWarnings("unused")
-    public MyIntentService(String name) {
+    public MyIntentService() {
         super(TAG);
     }
 
     @Override
     protected void onHandleIntent(@Nullable Intent intent) {
-        Toast.makeText(this, "MyIntentService.onHandleIntent", Toast.LENGTH_SHORT).show();
         Log.i("Eg:MyIntentService:24", "onHandleIntent ");
+
+        Intent intentActivity = new Intent(this, DetailActivity.class);
+        intentActivity.addFlags(FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intentActivity);
     }
 }
