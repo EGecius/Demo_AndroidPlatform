1b19c6110d24d4b2b41f0e29c8392d2c1bece59e
Egidijus Gecius <e.gecius@gmail.com>
2017-11-09 08:18:21 +0000

    show how context does not exist at the point of instantiation but does exist in onHandleIntent

--- app/src/main/java/com/egecius/demo_androidplatform/services/MyIntentService.java
+++ app/src/main/java/com/egecius/demo_androidplatform/services/MyIntentService.java
@@ -3,6 +3,7 @@ package com.egecius.demo_androidplatform.services;
 import static android.content.Intent.FLAG_ACTIVITY_NEW_TASK;
 
 import android.app.IntentService;
+import android.content.Context;
 import android.content.Intent;
 import android.support.annotation.Nullable;
 import android.util.Log;
@@ -19,12 +20,24 @@ public class MyIntentService extends IntentService {
     @SuppressWarnings("unused")
     public MyIntentService() {
         super(TAG);
+        Log.i("Eg:MyIntentService:23", "MyIntentService ");
+        printContext();
+    }
+
+    private void printContext() {
+        Context context = getBaseContext();
+        Log.i("Eg:MyIntentService:28", "printContext context " + context);
     }
 
     @Override
     protected void onHandleIntent(@Nullable Intent intent) {
         Log.i("Eg:MyIntentService:24", "onHandleIntent ");
+        printContext();
 
+        startActivity();
+    }
+
+    private void startActivity() {
         Intent intentActivity = new Intent(this, DetailActivity.class);
         intentActivity.addFlags(FLAG_ACTIVITY_NEW_TASK);
         startActivity(intentActivity);
