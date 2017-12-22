179dc4fe86a6553891ee28e86edfb2834a6e7c44
Egidijus Gecius <e.gecius@gmail.com>
2017-12-22 11:45:41 +0000

    show fragment 1

--- app/src/main/java/com/egecius/demo_androidplatform/fragments/MyFragmentActivity.java
+++ app/src/main/java/com/egecius/demo_androidplatform/fragments/MyFragmentActivity.java
@@ -3,22 +3,25 @@ package com.egecius.demo_androidplatform.fragments;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.FragmentActivity;
-import android.widget.FrameLayout;
 
 import com.egecius.demo_androidplatform.R;
 
 public class MyFragmentActivity extends FragmentActivity {
 
-    private FrameLayout fragmentContainer;
-
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_fragment);
-        findViews();
+        addFragment1();
     }
 
-    private void findViews() {
-        fragmentContainer = findViewById(R.id.container);
+    private void addFragment1() {
+
+        Fragment1 fragment = new Fragment1();
+
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.container, fragment)
+                .addToBackStack(null)
+                .commit();
     }
 }
