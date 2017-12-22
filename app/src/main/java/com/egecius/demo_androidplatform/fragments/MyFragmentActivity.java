52386a0067daa889f259d43f4af09a6a81425a04
Egidijus Gecius <e.gecius@gmail.com>
2017-12-22 15:24:12 +0000

    add logs for backstack change

--- app/src/main/java/com/egecius/demo_androidplatform/fragments/MyFragmentActivity.java
+++ app/src/main/java/com/egecius/demo_androidplatform/fragments/MyFragmentActivity.java
@@ -4,7 +4,9 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.util.Log;
 
 import com.egecius.demo_androidplatform.R;
 
@@ -14,15 +16,33 @@ public class MyFragmentActivity extends FragmentActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_fragment);
+        addBackStackListener();
         addFragment1();
     }
 
+    private void addBackStackListener() {
+        getSupportFragmentManager().addOnBackStackChangedListener(
+                new FragmentManager.OnBackStackChangedListener() {
+                    @Override
+                    public void onBackStackChanged() {
+                        Log.i("Eg:MyFragmentActivity:27", "onBackStackChanged ");
+                    }
+                });
+    }
+
     private void addFragment1() {
-        replaceFragment(new Fragment1(), false);
+        Log.v("Eg:MyFragmentActivity:35", "addFragment1");
+        replaceFragment(new Fragment1(), true);
     }
 
     void showFragment2() {
+        Log.v("Eg:MyFragmentActivity:40", "showFragment2");
         replaceFragment(new Fragment2(), false);
+    }
+
+    void showFragment3() {
+        Log.v("Eg:MyFragmentActivity:45", "showFragment3");
+        replaceFragment(new Fragment3(), false);
     }
 
     private void replaceFragment(Fragment fragment, boolean useBackStack) {
@@ -35,9 +55,5 @@ public class MyFragmentActivity extends FragmentActivity {
         }
 
         transaction.commit();
-    }
-
-    void showFragment3() {
-        replaceFragment(new Fragment3(), false);
     }
 }
