0c629a6cfe510db7d4e3f03a20cff8aa67898168
Egidijus Gecius <e.gecius@gmail.com>
2018-04-12 08:39:20 +0100

    convert MainActivity to Kotlin

---
+++ app/src/main/java/com/egecius/demo_androidplatform/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.egecius.demo_androidplatform
+
+import android.content.Intent
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+
+import com.egecius.demo_androidplatform.services.MyIntentService
+
+class MainActivity : AppCompatActivity() {
+
+    private val mNotificationSender = NotificationSender(
+            MyApplication.appContext)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        setOnClickListener()
+    }
+
+    private fun setOnClickListener() {
+        findViewById<View>(R.id.start_intent_service).setOnClickListener { startIntentService() }
+        findViewById<View>(R.id.send_notification).setOnClickListener { mNotificationSender.sendNotification() }
+    }
+
+    private fun startIntentService() {
+        val intent = Intent(applicationContext, MyIntentService::class.java)
+        startService(intent)
+    }
+}
