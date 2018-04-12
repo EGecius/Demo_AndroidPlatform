9034c5638becebf709f9754a617f86d3373b9c0d
Egidijus Gecius <e.gecius@gmail.com>
2018-04-12 08:42:26 +0100

    convet MyApplication to kotlin

---
+++ app/src/main/java/com/egecius/demo_androidplatform/MyApplication.kt
@@ -0,0 +1,18 @@
+package com.egecius.demo_androidplatform
+
+import android.app.Application
+import android.content.Context
+
+class MyApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        appContext = applicationContext
+    }
+
+    companion object {
+
+        var appContext: Context? = null
+            private set
+    }
+}
