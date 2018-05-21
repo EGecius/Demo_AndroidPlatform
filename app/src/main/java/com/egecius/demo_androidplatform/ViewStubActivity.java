8aecced753c74122f43cb83a14faeda1962938a2
Egidijus Gecius <>
2018-05-21 15:03:30 +0100

    inject into ViewStub

--- app/src/main/java/com/egecius/demo_androidplatform/ViewStubActivity.java
+++ app/src/main/java/com/egecius/demo_androidplatform/ViewStubActivity.java
@@ -3,6 +3,7 @@ package com.egecius.demo_androidplatform;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.ViewStub;
 
 public class ViewStubActivity extends AppCompatActivity {
 
@@ -10,5 +11,13 @@ public class ViewStubActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_stub);
+
+        inflateIntoViewStub();
+    }
+
+    private void inflateIntoViewStub() {
+        ViewStub viewStub = findViewById(R.id.my_view_stub);
+        viewStub.setLayoutResource(R.layout.layout_to_inject);
+        viewStub.inflate();
     }
 }
