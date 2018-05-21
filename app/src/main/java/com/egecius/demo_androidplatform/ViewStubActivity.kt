1285c4512056b3e1ae58abc83978d531bbafdb17
Egidijus Gecius <>
2018-05-21 15:09:26 +0100

    add inflated id

--- app/src/main/java/com/egecius/demo_androidplatform/ViewStubActivity.kt
+++ app/src/main/java/com/egecius/demo_androidplatform/ViewStubActivity.kt
@@ -2,6 +2,8 @@ package com.egecius.demo_androidplatform
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import android.view.View
 import android.view.ViewStub
 
 class ViewStubActivity : AppCompatActivity() {
@@ -11,6 +13,14 @@ class ViewStubActivity : AppCompatActivity() {
         setContentView(R.layout.activity_view_stub)
 
         inflateIntoViewStub()
+        tryToFindViewStubByOldName()
+    }
+
+    private fun tryToFindViewStubByOldName() {
+        val viewStubByOldId = findViewById<ViewStub>(R.id.my_view_stub)
+        Log.v("Eg:ViewStubActivity:19", "tryToFindViewStubByOldName viewStubByOldId $viewStubByOldId")
+        val inflatedId = findViewById<View>(R.id.id_of_injected_view)
+        Log.v("Eg:ViewStubActivity:23", "tryToFindViewStubByOldName $inflatedId")
     }
 
     private fun inflateIntoViewStub() {
