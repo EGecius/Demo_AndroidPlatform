ea0fb2c87819409e1d9aeb4f91a0306e258d3a83
Egidijus Gecius <>
2018-05-21 14:54:49 +0100

    add empty ViewStub Activity

---
+++ app/src/main/java/com/egecius/demo_androidplatform/ViewStubActivity.java
@@ -0,0 +1,14 @@
+package com.egecius.demo_androidplatform;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class ViewStubActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_stub);
+    }
+}
