d002caf0022997dd71f33b2371acc82735a9da4c
Egidijus Gecius <>
2018-06-19 12:26:22 +0100

    call jobFinished()

--- app/src/main/java/com/egecius/demo_androidplatform/services/MyJobService.kt
+++ app/src/main/java/com/egecius/demo_androidplatform/services/MyJobService.kt
@@ -3,6 +3,7 @@ package com.egecius.demo_androidplatform.services
 import android.annotation.SuppressLint
 import android.app.job.JobParameters
 import android.app.job.JobService
+import android.os.Handler
 import android.widget.Toast
 
 @SuppressLint("NewApi")
@@ -10,7 +11,14 @@ class MyJobService : JobService() {
 
     override fun onStartJob(params: JobParameters?): Boolean {
         Toast.makeText(this, "onStartJob", Toast.LENGTH_SHORT).show()
+        scheduleJobFinished(params)
         return true // needs processing on a separate thread
+    }
+
+    private fun scheduleJobFinished(params: JobParameters?) {
+        Handler().postDelayed({
+            Toast.makeText(this, "jobFinished", Toast.LENGTH_SHORT).show()
+            jobFinished(params, false)}, 3000)
     }
 
     override fun onStopJob(params: JobParameters?): Boolean {
