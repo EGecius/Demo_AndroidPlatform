36b508b0aae08e44e3162445386d4dd820523702
Egidijus Gecius <>
2018-06-20 15:41:40 +0100

    simplify MyJobIntentService

--- app/src/main/java/com/egecius/demo_androidplatform/services/MyJobIntentService.java
+++ app/src/main/java/com/egecius/demo_androidplatform/services/MyJobIntentService.java
@@ -3,9 +3,7 @@ package com.egecius.demo_androidplatform.services;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Handler;
-import android.os.SystemClock;
 import android.support.v4.app.JobIntentService;
-import android.util.Log;
 import android.widget.Toast;
 
 public class MyJobIntentService extends JobIntentService {
@@ -21,39 +19,31 @@ public class MyJobIntentService extends JobIntentService {
 
     @Override
     protected void onHandleWork(Intent intent) {
-        // We have received work to do.  The system or framework is already
-        // holding a wake lock for us at this point, so we can just go.
-        Log.i("SimpleJobIntentService", "Executing work: " + intent);
-        String label = intent.getStringExtra("label");
-        if (label == null) {
-            label = intent.toString();
-        }
-        toast("Executing: " + label);
-        for (int i = 0; i < 5; i++) {
-            Log.i("SimpleJobIntentService", "Running service " + (i + 1)
-                    + "/5 @ " + SystemClock.elapsedRealtime());
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
+        sendToast("onHandleWork. Will sleep for 3s");
+        sleepFor3Secs();
+    }
+
+    private void sleepFor3Secs() {
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {}
+    }
+
+    // Helper for showing tests
+    void sendToast(final CharSequence text) {
+        mHandler.post(new Runnable() {
+            @Override
+            public void run() {
+                Toast.makeText(MyJobIntentService.this, text, Toast.LENGTH_SHORT).show();
             }
-        }
-        Log.i("SimpleJobIntentService", "Completed service @ " + SystemClock.elapsedRealtime());
+        });
     }
 
     @Override
     public void onDestroy() {
         super.onDestroy();
-        toast("All work complete");
+        sendToast("onDestroy");
     }
 
     final Handler mHandler = new Handler();
-
-    // Helper for showing tests
-    void toast(final CharSequence text) {
-        mHandler.post(new Runnable() {
-            @Override public void run() {
-                Toast.makeText(MyJobIntentService.this, text, Toast.LENGTH_SHORT).show();
-            }
-        });
-    }
 }
