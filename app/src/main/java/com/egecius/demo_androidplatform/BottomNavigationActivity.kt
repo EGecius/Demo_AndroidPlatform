0eb49677abe770fb42f0d88ae92b9c539e0b5031
Egidijus Gecius <>
2019-05-08 11:38:04 +0100

    add basic home navigation

---
+++ app/src/main/java/com/egecius/demo_androidplatform/BottomNavigationActivity.kt
@@ -0,0 +1,13 @@
+package com.egecius.demo_androidplatform
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class BottomNavigationActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_botton_navigation)
+    }
+
+}
