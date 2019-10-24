ba6761b33371a1e9f83ed239f3e88db2ac63911e
Egidijus Gecius <e.gecius@gmail.com>
2019-10-24 14:46:46 +0100

    add initial bottom sheet

---
+++ app/src/main/java/com/egecius/demo_androidplatform/bottomsheet/BottomSheetActivity.kt
@@ -0,0 +1,15 @@
+package com.egecius.demo_androidplatform.bottomsheet
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.egecius.demo_androidplatform.R
+
+class BottomSheetActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_bottom_sheet)
+    }
+
+
+}
