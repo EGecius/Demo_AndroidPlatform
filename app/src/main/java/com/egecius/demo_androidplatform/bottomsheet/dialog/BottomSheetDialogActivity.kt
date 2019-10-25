c82dee0f030ecac74ee8df1b00a2e256aacf8d2a
Egidijus Gecius <e.gecius@gmail.com>
2019-10-25 11:05:12 +0100

    add example how to open bottom sheet dialog

---
+++ app/src/main/java/com/egecius/demo_androidplatform/bottomsheet/dialog/BottomSheetDialogActivity.kt
@@ -0,0 +1,37 @@
+package com.egecius.demo_androidplatform.bottomsheet.dialog
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.egecius.demo_androidplatform.R
+import com.google.android.material.bottomsheet.BottomSheetDialog
+import kotlinx.android.synthetic.main.activity_bottom_sheet_dialog.*
+import kotlinx.android.synthetic.main.bottom_sheet.view.*
+
+
+class BottomSheetDialogActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_bottom_sheet_dialog)
+        setOnClickListeners()
+    }
+
+    private fun setOnClickListeners() {
+        open.setOnClickListener {
+            showBottomSheetDialog()
+        }
+    }
+
+    @SuppressLint("LongLogTag")
+    private fun showBottomSheetDialog() {
+        val dialog = BottomSheetDialog(this)
+        val sheetView = layoutInflater.inflate(R.layout.bottom_sheet, null)
+        sheetView.take_action.setOnClickListener {
+            Log.v("Eg:BottomSheetDialogActivity:32", "showBottomSheetDialog onClicked 'take action'")
+        }
+        dialog.setContentView(sheetView)
+        dialog.show()
+    }
+}
