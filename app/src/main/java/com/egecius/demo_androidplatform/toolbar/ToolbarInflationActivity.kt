846cc047996dedac7dc4090bf06fff7023686e44
Egidijus Gecius <e.gecius@gmail.com>
2020-05-29 07:58:25 +0100

    set action bar

--- app/src/main/java/com/egecius/demo_androidplatform/toolbar/ToolbarInflationActivity.kt
+++ app/src/main/java/com/egecius/demo_androidplatform/toolbar/ToolbarInflationActivity.kt
@@ -3,12 +3,18 @@ package com.egecius.demo_androidplatform.toolbar
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.egecius.demo_androidplatform.R
+import kotlinx.android.synthetic.main.activity_toolbar_inflation.*
 
 class ToolbarInflationActivity: AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_toolbar_inflation)
+        setToolbar()
+    }
+
+    private fun setToolbar() {
+        setSupportActionBar(toolbar)
     }
 
 }
