128c58f02229e488bef28c51eca58c317ebb653b
Egidijus Gecius <e.gecius@gmail.com>
2020-05-29 07:54:18 +0100

    fix ToolbarInflationActivity.kt

--- app/src/main/java/com/egecius/demo_androidplatform/toolbar/ToolbarInflationActivity.kt
+++ app/src/main/java/com/egecius/demo_androidplatform/toolbar/ToolbarInflationActivity.kt
@@ -2,12 +2,13 @@ package com.egecius.demo_androidplatform.toolbar
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.egecius.demo_androidplatform.R
 
 class ToolbarInflationActivity: AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        setContentView(R.layout.activity_toolbar_inflation)
     }
 
 }
