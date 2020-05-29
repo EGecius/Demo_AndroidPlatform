cb59d69712f3ff32259e3f86f4b2f50dcf6f6fa1
Egidijus Gecius <e.gecius@gmail.com>
2020-05-29 07:50:18 +0100

    add empty ToolbarInflationActivity

---
+++ app/src/main/java/com/egecius/demo_androidplatform/toolbar/ToolbarInflationActivity.kt
@@ -0,0 +1,13 @@
+package com.egecius.demo_androidplatform.toolbar
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ToolbarInflationActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+}
