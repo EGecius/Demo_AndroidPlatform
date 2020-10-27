1b3da4920e96db11dc14baee1f4734bdb7849db5
Egidijus Gecius <e.gecius@gmail.com>
2020-10-27 10:45:33 +0000

    add TextView.highlightFirstWord()

--- app/src/main/java/com/egecius/demo_androidplatform/shared/extensions/ViewExtensions.kt
+++ app/src/main/java/com/egecius/demo_androidplatform/shared/extensions/ViewExtensions.kt
@@ -1,10 +1,15 @@
 package com.egecius.demo_androidplatform.shared.extensions
 
 import android.app.Activity
+import android.text.SpannableString
+import android.text.Spanned
+import android.text.style.AbsoluteSizeSpan
+import android.text.style.ForegroundColorSpan
 import android.view.View
 import android.view.inputmethod.EditorInfo
 import android.view.inputmethod.InputMethodManager
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 
 fun TextView.setKeyboardDoneButtonClickListener(listener: () -> Unit) {
 
@@ -22,4 +27,18 @@ fun TextView.setKeyboardDoneButtonClickListener(listener: () -> Unit) {
 fun View.hideKeyboard() {
     val inputMethodManager = context.getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
     inputMethodManager.hideSoftInputFromWindow(windowToken, 0)
+}
+
+fun TextView.highlightFirstWord(textSizeDp: Int? = null, highlightColor: Int? = null) {
+    this.text = SpannableString(text).apply {
+        val start = 0
+        val end = text.indexOf(' ')
+        textSizeDp?.also {
+            setSpan(AbsoluteSizeSpan(textSizeDp, true), start, end, Spanned.SPAN_INCLUSIVE_INCLUSIVE)
+        }
+        highlightColor?.also {
+            val color = ContextCompat.getColor(context, highlightColor)
+            setSpan(ForegroundColorSpan(color), start, end, Spanned.SPAN_INCLUSIVE_INCLUSIVE)
+        }
+    }
 }
